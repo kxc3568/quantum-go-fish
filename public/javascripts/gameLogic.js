ef7a34d75308b7ece0a2821c2922849330173355
Kevin Chen <kchen3568@gmail.com>
2020-05-31 18:39:28 -0500

    Added documentation of client-side functions and parameters

--- public/javascripts/gameLogic.js
+++ public/javascripts/gameLogic.js
@@ -1,9 +1,133 @@
-//
-// SOCKET EMISSION FUNCTIONS
-//
+/*
+    TABLE OF CONTENTS
+
+    1. UTILITY FUNCTIONS
+    2. VIEW CHANGE FUNCTIONS
+    3. FUNCTIONS THAT EMIT TO SERVER
+    4. FUNCTIONS THAT PROCESS SERVER COMMUNICATION
+
+*/
+
+/*
+    UTILITY FUNCTIONS
+*/
 
 /**
- * Changes to the view of the game lobby
+ * Removes all the children of the given HTML element with the given class;
+ * removes all children if class is empty string.
+ * @param {HTMLElement} el      The element to have it's children removed
+ * @param {String} class        The class to look for to remove
+ */
+const removeAllChildrenWithClass = (el, className) => {
+    while (el.lastElementChild && (className === "" || el.lastElementChild.classList.contains(className))) {
+        el.removeChild(el.lastElementChild);
+    }
+};
+
+/**
+ * Creates a paragraph node with the given text.
+ * @param {String} text     The text content of the paragraph node to be made
+ */
+const createRemovableTextNode = (text) => {
+    const p = document.createElement("p");
+    p.appendChild(document.createTextNode(text));
+    return p;
+};
+
+/*
+    VIEW CHANGE FUNCTIONS
+*/
+
+/**
+ * Listens to input changes in the create name to determine if the length is valid for the game
+ * to be created and sets the buttons activity based on that.
+ */
+const updateCreateButton = () => {
+    const createName = document.getElementById("create-name").value;
+    const createGameBtn = document.getElementById("create-game-btn");
+    if (createName.length >= 2 && createName.length <= 15) {
+        createGameBtn.removeAttribute("disabled");
+    } else {
+        createGameBtn.setAttribute("disabled", "true");
+    }
+};
+
+/**
+ * Listens to input changes in the join name to determine if the length is valid for the game
+ * to be created and sets the buttons activity based on that.
+ */
+const updateJoinButton = () => {
+    const joinName = document.getElementById("join-name").value;
+    const joinCode = document.getElementById("join-code").value;
+    const joinGameBtn = document.getElementById("join-game-btn");
+    if (joinName.length >= 2 && joinName.length <= 15 && joinCode.length === 4) {
+        joinGameBtn.removeAttribute("disabled");
+    } else {
+        joinGameBtn.setAttribute("disabled", "true");
+    }
+};
+
+/**
+ * Toggles the display of the scores.
+ */
+const toggleScores = () => {
+    const scores = document.getElementsByClassName("scores")[0];
+    if (scores.style.display === "none") {
+        scores.style.display = "";
+    } else {
+        scores.style.display = "none";
+    }
+};
+
+/**
+ * Toggles the display of game history.
+ */
+const toggleHistory = () => {
+    const history = document.getElementsByClassName("history")[0];
+    if (history.style.display === "none") {
+        history.style.display = "";
+    } else {
+        history.style.display = "none";
+    }
+};
+
+/**
+ * Changes display of all content container elements to none.
+ */
+const hideAll = () => {
+    let hide = Array.from(document.getElementsByClassName("content-container"));
+    hide.forEach(el => el.style.display = "none");
+};
+
+/**
+ * Changes from landing page view to creating game view.
+ */
+const createGameView = () => {
+    hideAll();
+    let toShow = document.getElementById("create-game-container");
+    toShow.style.display = "flex";
+};
+
+/**
+ * Changes from landing page view to joining game view.
+ */
+const joinGameView = () => {
+    hideAll();
+    let toShow = document.getElementById("join-game-container");
+    toShow.style.display = "flex";
+};
+
+/**
+ * Changes from creating or joining game view to landing view.
+ */
+const toHomeView = () => {
+    hideAll();
+    let toShow = document.getElementById("landing-container");
+    toShow.style.display = "flex";
+};
+
+/**
+ * Changes to the view of the game lobby.
  */
 const toLobbyView = () => {
     hideAll();
@@ -11,6 +135,9 @@ const toLobbyView = () => {
     toShow.style.display = "";
 };
 
+/**
+ * Changes from lobby view to the game view.
+ */
 const toStartView = () => {
     document.getElementsByClassName("hands-container")[0].style.display = "";
     document.getElementsByClassName("round-container")[0].style.display = "none";
@@ -20,7 +147,7 @@ const toStartView = () => {
 };
 
 /**
- * Shows the lobby or an error depending on data received from the server
+ * Shows the lobby or an error depending on data received from the server.
  * @param {Object} data     Contains details about the response of the join attempt
  */
 const processJoinResults = (data) => {
@@ -33,8 +160,12 @@ const processJoinResults = (data) => {
     }
 };
 
+/*
+    FUNCTIONS THAT EMIT TO SERVER
+*/
+
 /**
- * Determines if the nickname given is valid
+ * Determines if the nickname given is valid.
  * @param {String} nickname     The proposed nickname
  */
 const validName = (nickname) => {
@@ -45,8 +176,8 @@ const validName = (nickname) => {
 };
 
 /**
- * Pings the server socket to create a new Game and populates
- * the game code fields
+ * Pings the server socket to create a new Game and populates the game code fields if there is
+ * a valid name entered. Displays an error otherwise.
  */
 const createGame = () => {
     const nickname = document.getElementById("create-name").value;
@@ -68,8 +199,8 @@ const createGame = () => {
 };
 
 /**
- * Pings the server socket to add a new user to a Game and 
- * populates the game code fields
+ * Pings the server socket to add a new user to a Game and populates the game code fields if
+ * there is a valid name entered. Displays an error otherwise.
  */
 const joinGame = () => {
     const code = document.getElementById("join-code").value;
@@ -84,7 +215,8 @@ const joinGame = () => {
 };
 
 /**
- * Pings the server socket to start the game for all players
+ * Pings the server socket to start the game for all players if there are enough players. 
+ * Displays an error otherwise.
  */
 const startGame = () => {
     if (document.getElementById("player-list").children.length < 2) {
@@ -95,7 +227,7 @@ const startGame = () => {
 };
 
 /**
- * Pings the server to send a question in the socket's room
+ * Pings the server to send a question in the socket's room based on the decisions of the player.
  */
 const askQuestion = () => {
     const ps = document.getElementById("player-select");
@@ -106,7 +238,7 @@ const askQuestion = () => {
 };
 
 /**
- * Pings the server to respond to a question in the socket's room
+ * Pings the server to respond to a question in the socket's room based on the decision of the player.
  */
 const answerQuestion = () => {
     const rs = document.getElementById("response-select");
@@ -114,12 +246,25 @@ const answerQuestion = () => {
     socket.emit("Response", { response: response, responseFrom: socket.nickname });
 };
 
-//
-// SOCKET LISTENER FUNCTIONS
-//
+/*
+    FUNCTIONS THAT PROCESS SERVER COMMUNICATION
+
+    In order of socket events:
+    socket.on("Room Code", code => populateCode(code));
+    socket.on("Joined Game", data => processJoinResults(data));
+    socket.on("In Progress", showGameInProgress);
+    socket.on("Update Players", players => updatePlayerList(players));
+    socket.on("Game Started", toStartView);
+    socket.on("Update Hands", hands => updateHands(hands));
+    socket.on("Update Turn", player => updateTurn(player));
+    socket.on("Update History", history => updateHistory(history));
+    socket.on("Question", question => updateQuestion(question));
+    socket.on("Winner", winner => updateWinner(winner));
+    socket.on("Illegal Move", loser => updateLoser(loser));
+*/
 
 /**
- * Populates game code fields with the given code
+ * Populates game code fields with the given code.
  * @param {String} code     The code of the game
  */
 const populateCode = (code) => {
@@ -128,30 +273,18 @@ const populateCode = (code) => {
 };
 
 /**
- * Removes all the children of the given HTML element
- * @param {HTMLElement} el 
- * @param {String} class
+ * Displays an error that a game is currently in progress.
  */
-const removeAllChildrenWithClass = (el, className) => {
-    while (el.lastElementChild && (className === "" || el.lastElementChild.classList.contains(className))) {
-        el.removeChild(el.lastElementChild);
-    }
-};
-
-/**
- * Creates a paragraph node with the given text
- * @param {String} text 
- */
-const createRemovableTextNode = (text) => {
-    const p = document.createElement("p");
-    p.appendChild(document.createTextNode(text));
-    return p;
-};
-
 const showGameInProgress = () => {
     document.getElementById("game-in-progress").style.display = "inline";
 };
 
+/**
+ * Adds the names and scores to the player's list and the score's list.
+ * @param {HTMLElement} playerList      The player list element
+ * @param {HTMLElement} scoresList      The score list element
+ * @param {Player[]} players            The list of players and their scores.
+ */
 const addPlayersAndScores = (playerList, scoresList, players) => {
     players.forEach((player) => {
         const playerItem = document.createElement("li");
@@ -163,7 +296,7 @@ const addPlayersAndScores = (playerList, scoresList, players) => {
 };
 
 /**
- * Updates the lobby player list with the given nickname
+ * Updates the lobby player list and the game score list with the given nickname (and scores).
  * @param {Player[]} players      The list of current players in the game
  */
 const updatePlayerList = (players) => {
@@ -187,6 +320,10 @@ const disableHistory = () => {
     historyDisabled = true;
 };
 
+/**
+ * Creates a player marker element for a player for the game display.
+ * @param {String} nickname     The name of the player to create an element for.
+ */
 const createPlayerName = (nickname) => {
     const playerName = document.createElement("div");
     playerName.classList.add("player-name");
@@ -197,7 +334,7 @@ const createPlayerName = (nickname) => {
 };
 
 /**
- * 
+ * Creates a card element for the game display.
  * @param {String} suit 
  * @param {String} status 
  */
@@ -211,6 +348,12 @@ const createCard = (suit, status) => {
     return card;
 };
 
+/**
+ * Creates card elements for the determined cards in the hand and appends them to the 
+ * hand element.
+ * @param {HTMLElement} playerHand      The hand element of the player
+ * @param {Object} determined           The determined cards in the hand
+ */
 const addDetermined = (playerHand, determined) => {
     Object.keys(determined).forEach((suit) => {
         for (let i = 0; i < determined[suit]; i++) {
@@ -219,6 +362,12 @@ const addDetermined = (playerHand, determined) => {
     });
 };
 
+/**
+ * Creates card elements for the undetermined cards in the hand and appends them to the 
+ * hand element.
+ * @param {HTMLElement} playerHand      The hand element of the player
+ * @param {Array[]} undetermined        The undetermined cards in the hand
+ */
 const addUndetermined = (playerHand, undetermined) => {
     undetermined.forEach(possibleSuits => {
         const undeterminedText = possibleSuits[0].join(", ");
@@ -228,6 +377,10 @@ const addUndetermined = (playerHand, undetermined) => {
     });
 };
 
+/**
+ * Creates the hand element for a player for the game display.
+ * @param {Hand} hand       The object that contains details about a player's hand
+ */
 const createPlayerHand = (hand) => {
     const playerHand = document.createElement("div");
     playerHand.classList.add("player-hand");
@@ -238,23 +391,23 @@ const createPlayerHand = (hand) => {
 };
 
 /**
- * Adds the list of cards in hand to the HTML element
+ * Adds the list of cards in hand to the HTML element.
  * @param {HTMLElement} el          The HTML Element to append hand markup to
  * @param {Object} hand             Contains an individual socket's nickname and hand
- * @param {Boolean} currentPlayer   Whether the hand is of this socket's player
+ * @param {Boolean} otherPlayer     Whether the hand is of another socket's player
  */
-const appendHandToElement = (el, hand, currentPlayer) => {
+const appendHandToElement = (el, hand, otherPlayer) => {
     const player = document.createElement("div");
     player.appendChild(createPlayerName(hand.nickname));
     player.appendChild(createPlayerHand(hand.hand));
-    if (currentPlayer) {
+    if (otherPlayer) {
         player.classList.add("other-player");
     }
     el.appendChild(player);
 };
 
 /**
- * Updates the page with hand markup from all players' hands
+ * Updates the page with hand markup from all players' hands.
  * @param {Object} hands    The hands of all players accessed by Socket ID
  */
 const updateHands = (hands) => {
@@ -273,7 +426,7 @@ const updateHands = (hands) => {
 };
 
 /**
- * Adds an option to a select element
+ * Adds an option to a select element.
  * @param {HTMLElement} select      The HTML select element to add the option to
  * @param {String} optionName       The name of the option element to add
  */
@@ -285,7 +438,7 @@ const addOption = (select, optionName) => {
 };
 
 /**
- * Adds the option names to the selector
+ * Adds the option names to the selector.
  * @param {HTMLElement} select      The HTML select element to add options to
  * @param {String[]} optionNames    The names of the option elements to add
  */
@@ -294,7 +447,7 @@ const addOptions = (select, optionNames) => {
 };
 
 /**
- * Adds options to the player and suit selectors
+ * Adds options to the player and suit selectors.
  */
 const populateSelectors = () => {
     const playerList = document.getElementById("player-list");
@@ -307,7 +460,7 @@ const populateSelectors = () => {
 };
 
 /**
- * Clears the response containers
+ * Clears the response containers.
  */
 const clearResponses = () => {
     document.getElementById("response-select-container").style.display = "none";
@@ -315,8 +468,8 @@ const clearResponses = () => {
 };
 
 /**
- * Updates the turn and question containers based on who's turn it is
- * @param {Player} player 
+ * Updates the turn and question containers based on who's turn it is.
+ * @param {Player} player       The player who's turn it is
  */
 const updateTurn = (player) => {
     clearResponses();
@@ -338,6 +491,10 @@ const updateTurn = (player) => {
     turnTextContainer.appendChild(createRemovableTextNode(turnText));
 };
 
+/**
+ * Returns a description of the event that occurred in entry.
+ * @param {Object} entry    An object details an event that occurred in the game
+ */
 const makeEntry = (entry) => {
     switch (entry.type) {
         case "Question":
@@ -355,6 +512,10 @@ const makeEntry = (entry) => {
     }
 };
 
+/**
+ * Updates the history display with the events that have occurred in the game.
+ * @param {Object[]} history    The events that have happened in the course of a game
+ */
 const updateHistory = (history) => {
     const historyContainer = document.getElementsByClassName("history")[0]; 
     removeAllChildrenWithClass(historyContainer, "");
@@ -369,13 +530,16 @@ const updateHistory = (history) => {
 };
 
 /**
- * Hides the question on the page
+ * Hides the question on the page.
  */
 const clearQuestion = () => {
     const questionContainer = document.getElementById("question-container");
     questionContainer.style.display = "none";
 };
 
+/**
+ * Shows the question's response for a player.
+ */
 const showResponseSelect = () => {
     const responseSelect = document.getElementById("response-select");
     if (responseSelect.children.length == 0) {
@@ -459,6 +623,9 @@ const socketInit = () => {
     socket.on("Illegal Move", loser => updateLoser(loser));
 };
 
+/**
+ * Initializes the display of the page on landing.
+ */
 const viewInit = () => {
     document.getElementsByClassName("hands-container")[0].style.display = "none";
     document.getElementById("game-container").style.display = "none";
